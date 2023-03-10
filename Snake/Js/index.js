37693298bd4ea7a04617850f0337a5facb531ac9
Deyvid Oliveira Silva <Deyvid4900@gmail.com>
2023-03-10 20:33:55 -0300

    adição de movimentação(Parcial) do personagem

--- Snake/Js/index.js
+++ Snake/Js/index.js
@@ -1,3 +1,6 @@
+
+// declarações de Variáveis
+
 const telaEscolhaNivel = document.getElementById("container")
 const cabeca = document.getElementById("cabeça")
 const AreaDeJogo = document.getElementById("AreaDeJogo")
@@ -7,42 +10,151 @@ const btnNivelTres = document.getElementById("b3")
 const btnJogar = document.getElementById("btnJogar")
 const tempoDeJogo = document.getElementById("tempo")
 
+const areaDeAtuacao = document.getElementById("AreaDeAtuacao")
+const larguraAreaDeAtuacao = areaDeAtuacao.offsetWidth;
+const alturaAreaDeAtuacao = areaDeAtuacao.offsetHeight;
+
+const personagem = document.getElementById("Personagem")
+
+
+const aside = document.getElementById("aside")
+const btnUp = document.getElementById("up")
+const btnLeft = document.getElementById("left")
+const btnRight = document.getElementById("right")
+const btnDown = document.getElementById("down")
+
+
+
+let move=false
+
+//  Movimento
+btnUp.addEventListener("click", () => {
+
+    const addPx = () => {
+        let posicaoAtual = parseInt(personagem.style.bottom, 10) || 0
+
+        personagem.style.bottom = posicaoAtual + 5 + "px";
+        
+
+    }
+    if (move == false) {
+        setInterval(addPx, 50)
+     
+    }
+    
+
+})
+
+
+
+btnLeft.addEventListener("click", () => {
+
+    const addPx = () => {
+        let posicaoAtual = parseInt(personagem.style.left, 10) || 0
+
+        personagem.style.left = posicaoAtual - 5 + "px";
+    }
+    if (move == false) {
+        setInterval(addPx, 50)
+        
+    }
+})
+
+
+
+btnRight.addEventListener("click", () => {
+
+
+    const addPx = () => {
+        let posicaoAtual = parseInt(personagem.style.left, 10) || 0
+
+        personagem.style.left = posicaoAtual + 5 + "px";
+    }
+    if (move == false) {
+        setInterval(addPx, 50)
+       
+    }
+})
+
+
+
+btnDown.addEventListener("click", () => {
+
+
+    const addPx = () => {
+        let posicaoAtual = parseInt(personagem.style.bottom, 10) || 0
+        personagem.style.bottom = posicaoAtual - 5 + "px";
+
+
+    }
+    if (move == false) {
+        setInterval(addPx, 50)
+        
+    }
+
+})
+
+
+
+
+
+
+// ticks
 
 let t = 0
 const tempoJogo = () => {
     tempoDeJogo.innerHTML = t
     t = t + 1
 }
-// const criarAleatorio = () => {
-
-//     var randomLeft = Math.floor(Math.random() * 1000);
-//     var randomTop = Math.floor(Math.random() * 1000);
-
-//     var newDiv = document.createElement("div");
-
-//     newDiv.className = "new-div";
-//     newDiv.style.top = randomTop + "px";
-//     newDiv.style.left = randomLeft + "px";
-    
-//     newDiv.setAttribute("class", "ObjPonto")
-
-//     AreaDeJogo.appendChild(newDiv);
-// }
 
 
 
 
 
 
+// Niveis do jogo
 
-
-
+btnNivelUm.addEventListener("click", () => {
+    telaEscolhaNivel.setAttribute("class", "Apagado")
+    cabeca.setAttribute("class", "Acesso")
+    AreaDeJogo.setAttribute("class", "Acesso")
+    aside.setAttribute("class", "Acesso")
+    setInterval(tempoJogo, 1000)
+    // setInterval(criarAleatorio, 500)
+    console.log(larguraAreaDeAtuacao)
+    console.log(alturaAreaDeAtuacao)
+})
+btnNivelDois.addEventListener("click", () => {
+    telaEscolhaNivel.setAttribute("class", "Apagado")
+    cabeca.setAttribute("class", "Acesso")
+    AreaDeJogo.setAttribute("class", "Acesso")
+    aside.setAttribute("class", "Acesso")
+    setInterval(tempoJogo, 1000)
+    // setInterval(criarAleatorio, 500)
+    console.log(larguraAreaDeAtuacao)
+    console.log(alturaAreaDeAtuacao)
+})
+btnNivelTres.addEventListener("click", () => {
+    telaEscolhaNivel.setAttribute("class", "Apagado")
+    cabeca.setAttribute("class", "Acesso")
+    AreaDeJogo.setAttribute("class", "Acesso")
+    aside.setAttribute("class", "Acesso")
+    setInterval(tempoJogo, 1000)
+    // setInterval(criarAleatorio, 500)
+    console.log(larguraAreaDeAtuacao)
+    console.log(alturaAreaDeAtuacao)
+})
 
 btnJogar.addEventListener("click", () => {
     telaEscolhaNivel.setAttribute("class", "Apagado")
     cabeca.setAttribute("class", "Acesso")
     AreaDeJogo.setAttribute("class", "Acesso")
+    aside.setAttribute("class", "Acesso")
     setInterval(tempoJogo, 1000)
     // setInterval(criarAleatorio, 500)
+    console.log(larguraAreaDeAtuacao)
+    console.log(alturaAreaDeAtuacao)
 })
 
+
+//Classes 
